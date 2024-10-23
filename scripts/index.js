668615eac7407468cd1a9826753971d9078bc611
lsebastian1991@gmail.com <seu.email@example.com>
2024-10-23 22:08:05 +0100

    add styles to btn and define page structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const API_KEY = "c780a7f95abdda4f5ccb13af8a3ce551";
 const weatherAPI = new moodyWeatherAPI(API_KEY);
 
-async function fetchAndLogWeather() {
+async function fetchWeather() {
   try {
     const temp = await weatherAPI.getWeatherData("London");
     console.log(`Temperature in ${city} is: ${temp}°C`);
@@ -10,4 +10,4 @@ async function fetchAndLogWeather() {
   }
 }
 
-fetchAndLogWeather();
+fetchWeather();
